7cb83a97b28d69b6f0dd8abc9f5bda304ce90b56
rradzzio <rradzzio@gmail.com>
2022-06-17 10:16:17 +0200

    add news/article banner with cached network image

--- lib/app/pages/user/user_home.dart
+++ lib/app/pages/user/user_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_app/app/providers.dart';
+import 'package:news_app/widgets/main_article.dart';
 import 'package:news_app/widgets/user_top_bar.dart';
 
 class UserHome extends ConsumerWidget {
@@ -9,16 +10,28 @@ class UserHome extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      //backgroundColor: const Color(0xFFFBFBFF),
+      backgroundColor: Color.fromARGB(232, 251, 252, 255),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               UserTopBar(leadingIconButton: IconButton(
                 icon: const Icon(Icons.logout),
                 onPressed: () => ref.read(firebaseAuthProvider).signOut(),
               ),),
               const SizedBox(height: 20,),
+              const Text(
+                'Your daily read',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 10,),
+              const MainArticle()
             ],
           ),
         )
---
+++ lib/widgets/main_article.dart
@@ -0,0 +1,77 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class MainArticle extends StatelessWidget {
+  const MainArticle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: const Color.fromARGB(75, 255, 255, 255),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: CachedNetworkImage(
+              imageUrl: 'https://i.imgur.com/5LY1mTO.jpeg',
+              key: UniqueKey(),
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Container(color: Colors.black12,),
+              errorWidget: (context, url, error) => const SizedBox(
+                child: Icon(
+                  Icons.broken_image,
+                  size: 100,
+                  color: Colors.black38,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                'Articles title is here and now!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0
+                  ),
+                ),
+                const SizedBox(height: 8,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const[
+                    Text(
+                      'Technology',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 149, 149, 158),
+                        fontSize: 14.0
+                      ),
+                    ),
+                    Text(
+                      '29.06.2022',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 149, 149, 158),
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14.0
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
+
+//design from https://dribbble.com/shots/14905782/attachments/6619453?mode=media
